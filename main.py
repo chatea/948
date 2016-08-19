81a858991bbd8788080759cc74520c47cfb057b8
chatea (Charlie Tsai) <cha122977@gamil.com>
2016-08-19 16:38:45 +0800

    Set debug mode and /test path for testing

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from flask import Flask
+import json
 import menu
 
 app = Flask(__name__)
+app.debug = True
 
 @app.route("/")
 def home():
@@ -12,12 +14,13 @@ def home():
 def page_not_found(e):
     return "404: Sorry, there is not such page!"
 
+
 @app.errorhandler(500)
 def page_not_found(e):
     return "500: Sorry, some bug happend!!!"
 
-
 if app.debug:
     @app.route("/test")
     def test():
-        return menu.get_menu(-1)
+        target_menu = menu.get_menu(u'-1')
+        return json.dumps(target_menu)
