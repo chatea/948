7cc612caa5560b26d4cfd86ae81887de62bae15a
chatea (Charlie Tsai) <cha122977@gamil.com>
2016-08-21 01:53:04 +0800

    provide get prices API

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from flask import Flask
+from flask import request
 import json
 import menu
+import optimizer
 
 app = Flask(__name__)
 app.debug = True
@@ -14,10 +16,12 @@ MENU_NAME_ID_MAP = {
 def home():
     return "Hello 948, Just Buy!"
 
+
 @app.route("/menus")
 def menus():
     menulist = menu.get_menu_list()
     return json.dumps(menulist)
+
 
 @app.route("/menus/<path:menu_name>")
 def get_menu(menu_name):
@@ -28,6 +32,21 @@ def get_menu(menu_name):
     target_menu = menu.get_menu(menu_id)
     return json.dumps(target_menu)
 
+
+@app.route("/getPrices",  methods=['GET'])
+def get_prices():
+    menu_id = request.args.get(u'menu_id')
+    orders = json.loads(request.args.get(u'orders'))
+    orders = [unicode(str(order), "utf-8") for order in orders]
+    print menu_id, orders
+    order_method = optimizer.calculate(menu_id, orders)
+    result = {
+        u"items":order_method.orders,
+        u"prices":order_method.prices
+    }
+    return json.dumps(result)
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return "404: Sorry, there is not such page!"
@@ -36,6 +55,7 @@ def page_not_found(e):
 @app.errorhandler(500)
 def page_not_found(e):
     return "500: Sorry, some bug happend!!!"
+
 
 if app.debug:
     @app.route("/test")
