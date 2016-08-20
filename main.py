9ffac6da57e626435616ce47b22a7d6230b175cc
chatea (Charlie Tsai) <cha122977@gamil.com>
2016-08-21 01:33:42 +0800

    Add menu list and menu items APIs

--- main.py
+++ main.py
@@ -5,10 +5,28 @@ import menu
 app = Flask(__name__)
 app.debug = True
 
+MENU_NAME_ID_MAP = {
+    u'test': u'-1',
+    u'mcdonald': u'0',
+}
+
 @app.route("/")
 def home():
     return "Hello 948, Just Buy!"
 
+@app.route("/menus")
+def menus():
+    menulist = menu.get_menu_list()
+    return json.dumps(menulist)
+
+@app.route("/menus/<path:menu_name>")
+def get_menu(menu_name):
+    if menu_name not in MENU_NAME_ID_MAP:
+        return "{}"
+
+    menu_id = MENU_NAME_ID_MAP[menu_name]
+    target_menu = menu.get_menu(menu_id)
+    return json.dumps(target_menu)
 
 @app.errorhandler(404)
 def page_not_found(e):
